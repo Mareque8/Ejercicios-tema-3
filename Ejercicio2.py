391096da553ffad981874993aae4aa3cddf62595
Mareque8 <mareque8marcos@gmail.com>
2022-10-31 14:04:01 +0100

    jjbkb

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -11,3 +11,13 @@ def insertar(a,b,c,d):
     matriz[1] [0] = c
     matriz [1] [1] = d
     return matriz
+
+def grado2(m2):
+    return m2[0][0] * m2[1][1] - m2[0][1] * m2[1][0]
+
+def grado3(m3):
+    A1 = m3[0][0] * grado2(insertar(m3[1][1],m3[1][2],m3[2][1],m3[2][2]))
+    A2 = m3[0][1] * grado2(insertar(m3[1][0],m3[1][2],m3[2][0],m3[2][2]))
+    A3 = m3[0][0] * grado2(insertar(m3[1][0],m3[1][1],m3[2][0],m3[2][1]))
+    return A1 - A2 + A3
+    
