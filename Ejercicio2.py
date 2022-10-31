cf961872306b9f42412867ddebcc2c0a4bdcfd50
Mareque8 <mareque8marcos@gmail.com>
2022-10-31 13:57:16 +0100

    kjj

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,6 +1,13 @@
-def matriz(filas,columnas):
+def matriz1(filas,columnas):
     matriz = [None] * filas
     for i in range(filas):
         matriz[i] = [None] * columnas
     return matriz
-    
+
+def insertar(a,b,c,d):
+    matriz = matriz1(2,2)
+    matriz[0] [0] = a
+    matriz [0] [1] = b
+    matriz[1] [0] = c
+    matriz [1] [1] = d
+    return matriz
