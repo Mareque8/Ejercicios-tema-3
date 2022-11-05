628bb650bbef4a49ae5bc1d59b256cc221bd77c2
Mareque8 <mareque8marcos@gmail.com>
2022-11-05 23:40:58 +0100

    enfkefn

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -12,4 +12,18 @@ def imprimir(lista):
     print("nombre, largo, tripulacion, pasajero")
     for i in lista: 
         print(i)
-         
+
+def imprimir2(lista, nombre):
+    for i in lista:
+        if i.nombre == nombre:
+            print(i)
+def imprimir3(lista, longitud):
+    for i in range(longitud):
+        print(lista[i])
+
+def imprimir4(lista,n):
+    for i in lista:
+        if i.pasajero >= n:
+            print(i)
+
+
