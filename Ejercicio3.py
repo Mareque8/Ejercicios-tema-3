32c1d28e021273f6ba1873c142de5fdd382aa7bf
Mareque8 <mareque8marcos@gmail.com>
2022-11-06 01:29:43 +0100

    ihih

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -25,5 +25,35 @@ def imprimir4(lista,n):
     for i in lista:
         if i.pasajero >= n:
             print(i)
+def imprimir5(lista,n):
+    contador = 0
+    for i in lista:
+        if i.nombre[0] == n[0] and i.nombre[1] ==n[1]:
+            print(i)
+            contador += 1
+    if contador == 0:
+        print("no hay naves que empiecen por esa letra")
+
+
+lista = []
+a = nave("Halcon Milenario", 100, 500, 350)
+lista.append(a)
+b = nave("Estrella de la muerte", 300, 1000, 871)
+lista.append(b)
+c = nave("Ala-X", 80,100, 50)
+lista.append(c)
+d = nave("Caza TIE", 90, 150, 100)
+lista.append(d)
+e = nave("Destructor Estelar", 300, 1200, 1000)
+lista.append(e)
+f = nave("Lanzadera imperial", 200, 200,200)
+lista.append(f)
+g = nave("Supremacy", 300, 280, 250)
+lista.append(g)
+h = nave("Nave de Control de Droides clase Lucrehulk", 290, 490, 460)
+lista.append(h)
+
+nn= sorted(lista, key = lambda x:x.nombre)
+imprimir(nn)
 
 
